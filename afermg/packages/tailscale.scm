dc8322ce32fff286949232f6c7650c6dbe5a213b
Alán F. Muñoz <afer.mg@gmail.com>
2024-05-22 20:15:34 -0400

    clean(tailscale): remove unused iproute

--- afermg/packages/tailscale.scm
+++ afermg/packages/tailscale.scm
@@ -24,7 +24,7 @@
         #~`((,(string-append "tailscale_" #$version "_amd64/") "/usr/bin/"))))
     (propagated-inputs
       ; iptables is required for setting up routing
-      (list iptables iproute))
+      (list iptables))
     (synopsis "Tailscale connects your team's devices and development environments for easy access to remote resources.")
     (description
      "Tailscale is a zero config VPN for building secure networks. Install on any device in minutes. Remote access from any network or physical location.")
